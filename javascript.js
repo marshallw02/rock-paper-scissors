8b6146f0595afbd3b1a3b9780abb23796142b624
marshallw02 <marshallw860@gmail.com>
2023-09-10 16:47:58 -0400

    Add getComputerChoice() to javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+function getComputerChoice() {
+    switch(Math.floor(Math.random() * 3)) {
+        case 0: return "Rock";
+        case 1: return "Paper";
+        case 2: return "Scissors";
+    }
+}
