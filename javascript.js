e2b72da906a603b9b78e0b1803839c4ec7e26965
marshallw02 <marshallw860@gmail.com>
2023-09-30 17:34:48 -0400

    Finish game() implementation

--- javascript.js
+++ javascript.js
@@ -19,6 +19,8 @@ function getPlayerChoice() {
     return pc.toLowerCase();
 }
 
+//Function will take both selections and determine the winner.
+//playRound() returns -1 if player loses, 0 if round is a tie, and 1 if player wins
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
     
@@ -26,54 +28,62 @@ function playRound(playerSelection, computerSelection) {
         case "rock":
             switch (computerSelection) {
                 case "rock":
-                    
-                    return "Tie!  You both played Rock!";
+                    console.log("Tie!  You both played Rock!")
+                    return 0;
                 case "paper":
-                    
-                    return "You Lose!  Paper beats Rock!";
+                    console.log("You Lose!  Paper beats Rock!")
+                    return -1;
                 case "scissors":
-                    
-                    return "You Win!  Rock beats Scissors!";
+                    console.log("You Win!  Rock beats Scissors!")
+                    return 1;
             }
             break;
         case "paper":
             switch (computerSelection) {
                 case "rock":
-                    
-                    return "You Win!  Paper beats Rock!";
+                    console.log("You Win!  Paper beats Rock!")
+                    return 1;
                 case "paper":
-                    
-                    return "Tie!  You both played Paper";
+                    console.log("Tie!  You both played Paper")
+                    return 0;
                 case "scissors":
-                    
-                    return "You Lose!  Scissors beat Paper!";
+                    console.log("You Lose!  Scissors beat Paper!")
+                    return -1;
             }
             break;
         case "scissors":
             switch (computerSelection) {
                 case "rock":
-                    
-                    return "You Lose!  Rock beats Scissors!";
+                    console.log("You Lose!  Rock beats Scissors!")
+                    return -1;
                 case "paper":
-                    
-                    return "You Win!  Scissors beat Paper!";
+                    console.log("You Win!  Scissors beat Paper!")
+                    return 1;
                 case "scissors":
-                    
-                    return "Tie!  You both played Scissors!";
+                    console.log("Tie!  You both played Scissors!")
+                    return 0;
             }
     }
 }
 
 function game() {
     let playerSelection, computerSelection; 
-    let result;
+    let roundResult, finalResult = 0;
 
     for (let i = 0; i < 5; i++) {
-        playerSelection = getPlayerChoice();
-        computerSelection = getComputerChoice();
-        result = playRound(playerSelection, computerSelection);
-        
+        roundResult = 0;
+        while (roundResult == 0){
+            playerSelection = getPlayerChoice();
+            computerSelection = getComputerChoice();
+            roundResult = playRound(playerSelection, computerSelection);
+        }
+        finalResult += roundResult;
     }
+
+    if (finalResult > 0)
+        return "Player wins!";
+    else
+        return "Computer wins!";
 }
 
-console.log(playRound(playerSelection, computerSelection));
+console.log(game());
